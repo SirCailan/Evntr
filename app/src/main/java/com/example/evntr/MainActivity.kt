fda221316642fe4ec4db9838e9b343771ede7edd
Smudex <99404643+Smudex@users.noreply.github.com>
2022-04-06 12:43:56 +0200

    buttons
    Mangler Back knapp

--- app/src/main/java/com/example/evntr/MainActivity.kt
+++ app/src/main/java/com/example/evntr/MainActivity.kt
@@ -2,13 +2,36 @@ package com.example.evntr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // #D49CF2 color to back event
 class MainActivity : AppCompatActivity() {
+
+    lateinit var main: FrameLayout
+    lateinit var mainScreen: MainScreen
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+<<<<<<< Updated upstream
 
+=======
+        main = findViewById(R.id.Main_Frame)
+        mainScreen = MainScreen()
+
+        chooseFragment(mainScreen)
+
+    }
+    fun chooseFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.Main_Frame, fragment).commit()
+>>>>>>> Stashed changes
     }
 }
--- app/src/main/java/com/example/evntr/MainFragment.kt
+++ app/src/main/java/com/example/evntr/MainFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainFragment : Fragment() {
+
+    lateinit var event: Button
+    lateinit var favorites: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +28,43 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_main_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        event = view.findViewById(R.id.Events_Button)
+        favorites = view.findViewById(R.id.Favoritt_Button)
+
+        event.setOnClickListener {
+            event()
+        }
+        favorites.setOnClickListener {
+            favorites()
+        }
+    }
+
+    fun event() {
+        CoroutineScope(Dispatchers.IO).launch {
+            activity?.supportFragmentManager?.commit {
+                setReorderingAllowed(false)
+                replace<EventScreen>(R.id.Main_Frame)
+            }
+        }
+    }
+
+    fun favorites() {
+        CoroutineScope(Dispatchers.IO).launch {
+            activity?.supportFragmentManager?.commit {
+                setReorderingAllowed(false)
+                replace<FavorittScreen>(R.id.Main_Frame)
+            }
+
+
+        }
+
+
+    }
 }
