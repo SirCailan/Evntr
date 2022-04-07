5eadd2fca68c19a55382b828814c2c58f74f6787
judith lourde <judith.lourde@gmail.com>
2022-04-07 10:13:18 +0200

    Add host and speaker files

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,13 +1,16 @@
-
 import createSchema from 'part:@sanity/base/schema-creator'
-
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+
 import event from './documents/event.js'
+import host from './documents/host.js'
+import speaker from './documents/speaker.js'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
     /* Your types here! */
-    event
+    event,
+    host,
+    speaker
   ]),
 })
