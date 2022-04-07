878127cd6fb9540ff3f0c324f822eac7b81eabd4
judith lourde <judith.lourde@gmail.com>
2022-04-07 11:09:00 +0200

    Fix host

--- studio/schemas/documents/event.js
+++ studio/schemas/documents/event.js
@@ -23,12 +23,6 @@ export default {
             validation: Rule => Rule.required(),
         },
         {
-            title: 'Host',
-            description: 'Company or Hosted by:',
-            name: 'host',
-            type: 'string'
-        },
-        {
             title: 'Category / Type of event',
             name: 'category',
             type: 'string'
