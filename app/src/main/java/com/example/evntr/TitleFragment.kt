262574ad5fbbbbb3091d06c107afadd33e2a4e84
SirCailan <99321082+SirCailan@users.noreply.github.com>
2022-04-07 15:46:34 +0200

    Deleted misc and added titlescreen

--- app/src/main/java/com/example/evntr/TitleFragment.kt
+++ app/src/main/java/com/example/evntr/TitleFragment.kt
@@ -6,17 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 
 class TitleFragment : Fragment() {
-
-    lateinit var loadingPhoto: Drawable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_title_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Thread.sleep(3000)
+
+        findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToMainFragment())
     }
 
 }
