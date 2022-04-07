497dcc025605f39a50be3ca83e54bbb9dc24b437
judith lourde <judith.lourde@gmail.com>
2022-04-07 10:15:12 +0200

    Add speaker schema

---
+++ studio/schemas/documents/speaker.js
@@ -0,0 +1,18 @@
+export default {
+    title: 'Speaker',
+    name: 'speaker',
+    type: 'document',
+    fields: [
+        {
+            title: 'Name',
+            name: 'name',
+            type: 'string',
+            description: 'Please use "Firstname Lastname" format',
+        },
+        {
+            title: 'Image',
+            name: 'image',
+            type: 'image',
+        }
+    ]
+}
